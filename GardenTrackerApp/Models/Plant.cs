9b05bc11a809cdc81863ea2b255ff99492f7670a
hvittorelli <misshopev@gmail.com>
2025-10-09 17:14:10 -0400

    Add project files.

---
+++ GardenTrackerApp/Models/Plant.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GardenTrackerApp.Models
+{
+    public class Plant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Type { get; set; }
+
+        [StringLength(100)]
+        public string Name {  get; set; }
+
+        [StringLength(50)]
+        public string WaterFrequency { get; set; }
+
+        [StringLength(500)]
+        public string Notes { get; set; }
+    }
+}
---
+++ GardenTrackerApp/Pages/Hope/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GardenTrackerApp.Data;
+using GardenTrackerApp.Models;
+
+namespace GardenTrackerApp.Pages.Hope
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly GardenTrackerApp.Data.GardenTrackerAppContext _context;
+
+        public DeleteModel(GardenTrackerApp.Data.GardenTrackerAppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Plant Plant { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plant = await _context.Plant.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (plant == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Plant = plant;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plant = await _context.Plant.FindAsync(id);
+            if (plant != null)
+            {
+                Plant = plant;
+                _context.Plant.Remove(Plant);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
